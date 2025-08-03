acb80226ef7bccc39287e44df2439dffb811bd95
Heemin2be <heemin7904@gmail.com>
2025-08-03 19:57:11 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
