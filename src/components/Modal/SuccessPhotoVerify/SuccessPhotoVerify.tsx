f621ebe65c0fe7c20bdb79c1f274f922c561b816
yunadada <ynson0216@naver.com>
2025-08-08 20:12:18 +0900

    feat: 사진 인증 성공 모달창 구현

---
+++ src/components/Modal/SuccessPhotoVerify/SuccessPhotoVerify.tsx
@@ -0,0 +1,33 @@
+import style from "./SuccessPhotoVerify.module.css";
+import { FiCheck } from "react-icons/fi";
+import ConfettiExplosion from "react-confetti-explosion";
+
+const SuccessPhotoVerify = () => {
+  return (
+    <div className={style.modalOverlay}>
+      <div className={style.wrapper}>
+        <div className={style.checkIcon}>
+          <FiCheck />
+        </div>
+        <h4>미션 인증 완료!</h4>
+        <p>
+          <strong>+100 리워드</strong>를 적립해드렸어요.
+        </p>
+        <div className={style.button}>
+          <button>미션 지도로 이동</button>
+          <button>리뷰 쓰기</button>
+        </div>
+      </div>
+      <div className={style.confetti}>
+        <ConfettiExplosion
+          force={0.7}
+          duration={2500}
+          particleCount={80}
+          width={1100}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SuccessPhotoVerify;
