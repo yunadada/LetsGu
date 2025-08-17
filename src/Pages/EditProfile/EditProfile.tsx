c871f7c5a5e94ce5209b1df301f04de39058991b
yunadada <ynson0216@naver.com>
2025-08-17 11:16:22 +0900

    feat: 정보 수정 페이지 UI 구현

---
+++ src/Pages/EditProfile/EditProfile.tsx
@@ -0,0 +1,27 @@
+import style from "./EditProfile.module.css";
+import Header from "../../components/Header/Header";
+import Profile from "../../components/MyPage/Profile/Profille";
+import InfoItem from "../../components/EditProfile/InfoItem/InfoItem";
+
+const EditProfile = () => {
+  return (
+    <div className={style.wrapper}>
+      <Header title="내 프로필" />
+      <div className={style.contents}>
+        <div className={style.profile}>
+          <Profile />
+        </div>
+        <div className={style.infoSection}>
+          <InfoItem label="이름" value="사용자" />
+          <InfoItem label="이메일" value="user@naver.com" />
+        </div>
+        <div className={style.infoSection}>
+          <InfoItem label="로그아웃" />
+          <InfoItem label="회원탈퇴" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditProfile;
