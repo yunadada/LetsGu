34d0d0851d6eccb5aac74b801c994a057964245c
yunadada <ynson0216@naver.com>
2025-08-20 01:41:02 +0900

    feat: 루트에 <ToastContainer> 적용하여 알림 기능 지원

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { Route, Routes } from "react-router-dom";
 import routes from "./router/router";
 const { publicRoutes, authenticateRoutes } = routes;
 import type { RouteConfig } from "./router/router";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const allRoutes: RouteConfig[] = [...publicRoutes, ...authenticateRoutes];
 
   return (
-    <Routes>
-      {allRoutes.map((route) => {
-        const { path, element } = route;
-        return <Route key={path} path={path} element={element} />;
-      })}
-    </Routes>
+    <div>
+      <ToastContainer position="top-center" autoClose={3000} limit={1} />
+      <Routes>
+        {allRoutes.map((route) => {
+          const { path, element } = route;
+          return <Route key={path} path={path} element={element} />;
+        })}
+      </Routes>
+    </div>
   );
 }
 
