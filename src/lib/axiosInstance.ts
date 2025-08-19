e82deca819a7a9f12d3d1c01cf7aef659e7084a1
yunadada <ynson0216@naver.com>
2025-08-19 16:20:15 +0900

    feat: axios 인스턴스 생성 및 요청 인터셉터로 Authorization 헤더 자동 추가

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_REACT_APP_BASE_URL,
+  headers: { "Content-Type": "application/json" },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
