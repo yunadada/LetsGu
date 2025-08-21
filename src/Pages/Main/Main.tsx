05a56cbfa90c3a03071e9fd78efb88868e7124a9
yunadada <ynson0216@naver.com>
2025-08-21 14:01:51 +0900

    refactor: 정적으로 작성한 날짜를 Date 객체로 대체하여 당일 날짜 자동 반영

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -9,6 +9,11 @@ import MissionStartThumbnail from "../../assets/MissionStartThumbnail.svg";
 const Main = () => {
   const mypageItems = ["활동내역", "내 지갑", "리워드 샵"];
 
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
   return (
     <div className={style.wrapper}>
       <div className={style.header}>
@@ -20,7 +25,9 @@ const Main = () => {
           <img src={ProfileImg} />
         </div>
       </div>
-      <p className={style.date}>2025년 08월 26일</p>
+      <p className={style.date}>
+        {year}년 {month}월 {day}일
+      </p>
       <Weather />
       <div className={style.navBar}>
         <NavItem
