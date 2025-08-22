225a47ba2695da64954c66a98e3d84d7dea64dab
yunadada <ynson0216@naver.com>
2025-08-23 02:29:45 +0900

    feat: 리뷰 작성 완료 모달창 구현

---
+++ src/components/Modal/SuccessReviewModal/SuccessReviewModal.tsx
@@ -0,0 +1,25 @@
+import { FiCheck } from "react-icons/fi";
+import { IoIosClose } from "react-icons/io";
+import style from "./SuccessReviewModal.module.css";
+import { Link } from "react-router-dom";
+
+const SuccessReviewModal = () => {
+  return (
+    <div className={style.modalOverlay}>
+      <div className={style.wrapper}>
+        <Link className={style.closeIcon} to="/activityLog">
+          <IoIosClose />
+        </Link>
+        <div className={style.checkIcon}>
+          <FiCheck />
+        </div>
+        <h4>리뷰 작성 완료!</h4>
+        <p>
+          <strong>+100 리워드</strong>를 적립해드렸어요.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessReviewModal;
