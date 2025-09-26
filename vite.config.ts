93ee8c0260cec40fc70198185a97442eb146ca9b
yunadada <ynson0216@naver.com>
2025-09-27 00:14:20 +0900

    chore: 개발 서버 포트번호 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     server: {
-      port: 5174,
+      port: 5173,
       proxy: {
         "/api": {
           target: env.VITE_API_PROXY_TARGET || "https://letsggu.duckdns.org",
